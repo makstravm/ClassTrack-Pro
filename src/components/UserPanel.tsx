a57e9b15d0888190870d3e285f3df37a1c879e3c
Maksym <maks7294@gmail.com>
2023-08-16 18:59:20 +0300

    feat: sign out user, added UI to the Header

---
+++ src/components/UserPanel.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import { Box, Button, Grid, Popover, Typography } from "@mui/material";
+import { signOutUser } from "../api/user";
+import { useUserContext } from "../context/userContext";
+
+export const UserPanel = () => {
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+  const { user } = useUserContext();
+  const handleClick = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const id = !!anchorEl ? "simple-popover" : undefined;
+
+  return (
+    <Grid item>
+      <Button
+        aria-describedby={id}
+        variant="text"
+        color="secondary"
+        onClick={handleClick}
+        sx={{ textTransform: "inherit" }}
+      >
+        <Typography component={"span"}>Hi,</Typography>
+        <Typography component={"span"} pl={1}>
+          {user?.displayName || user?.email}
+        </Typography>
+        <ArrowDropDownIcon />
+      </Button>
+      <Popover
+        id={id}
+        open={!!anchorEl}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+      >
+        <Box p={1}>
+          <Button variant="outlined" color="secondary" onClick={signOutUser}>
+            Sign out
+          </Button>
+        </Box>
+      </Popover>
+    </Grid>
+  );
+};
